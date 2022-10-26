31d9d64abad5cf0c9801ddbe361021c561c280cc
perintyler <perintyler@gmail.com>
2022-10-26 14:30:23 -0400

    add bash commands for clearing terminal and its scrollback

--- commands.sh
+++ commands.sh
@@ -1,1 +1,12 @@
 # tpkit/commands.sh
+
+function clear_scrollback()
+{
+    printf '\33c\e[3J'; # https://stackoverflow.com/questions/2198377/how-can-i-clear-previous-output-in-terminal-in-mac-os-x
+}
+
+function clearall() 
+{
+  clear;
+  clear_scrollback;
+}
