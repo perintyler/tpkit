d11ab9d3faf74adfb0b511f168bfa6f3f4909d24
perintyler <perintyler@gmail.com>
2022-10-26 14:22:31 -0400

    use the type.fit API to fetch a collection of quotes

--- quotes.py
+++ quotes.py
@@ -6,6 +6,9 @@ This module generates random quotes.
 """
 
 from dataclasses import dataclass
+import requests 
+
+QUOTE_API_URL = 'https://type.fit/api/quotes'
 
 @dataclass
 class Quote:
@@ -15,3 +18,8 @@ class Quote:
   def __str__(self):
     return f'{self.text}\n  - {self.author}'
 
+def fetch_quotes():
+  request = requests.get(QUOTE_API_URL)
+  if request.status_code != requests.codes.ok:
+    raise ConnectionError(f'quote API request failed with status code {request.status_code}')
+  return [Quote(**json_object) for json_object in request.json()]
