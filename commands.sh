90c86b255e66eee3f5993bc8e78a5ffa468578df
perintyler <perintyler@gmail.com>
2022-10-26 14:31:11 -0400

    command for opening preferred code editor

--- commands.sh
+++ commands.sh
@@ -1,5 +1,7 @@
 # tpkit/commands.sh
 
+PREFERRED_TEXT_EDITOR_FOR_CODING="sublime text" # application name
+
 function clear_scrollback()
 {
     printf '\33c\e[3J'; # https://stackoverflow.com/questions/2198377/how-can-i-clear-previous-output-in-terminal-in-mac-os-x
@@ -9,4 +11,9 @@ function clearall()
 {
   clear;
   clear_scrollback;
+}
+
+function lets_code() 
+{
+  open -a $PREFERRED_TEXT_EDITOR_FOR_CODING $1;
 }
