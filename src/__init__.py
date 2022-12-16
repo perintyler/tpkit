6c541824aae92b436aa928d4f6f1da5ca69a844f
perintyler <perintyler@gmail.com>
2022-12-16 15:06:37 -0500

    define package level public functions

--- src/__init__.py
+++ src/__init__.py
@@ -2,3 +2,18 @@
   tpkit
   ~~~~~
 """
+
+from .filelib import \
+(
+  iterate_paths, 
+  get_paths,
+  crawl
+)
+
+from .functional import map
+
+from .location import get_current_geocoordinate
+
+from .weather import get_current_weather
+
+from .quotes import get_random_quote
